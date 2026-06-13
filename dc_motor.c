6ffb22bd93ea0f85cf0e8cd399036d064ee5f42f
yoonsangpark <yoonsangpark@gmail.com>
2026-06-13 10:42:48 +0900

    Create dc_motor.c

---
+++ dc_motor.c
@@ -0,0 +1,30 @@
+#include <AFMotor.h>
+
+#include <AFMotor.h>
+
+AF_DCMotor motor(4);   // Port (1~4)
+
+void setup()
+{
+    Serial.begin(115200);
+    motor.setSpeed(200);  // Speed (0~255)
+}
+
+void loop()
+{
+    Serial.println(">> Loop ...");
+
+    Serial.println("FORWARD ...");
+    motor.run(FORWARD);
+    delay(3000);
+
+    motor.run(RELEASE);
+    delay(1000);
+
+    Serial.println("BACKWARD ...");
+    motor.run(BACKWARD);
+    delay(3000);
+
+    motor.run(RELEASE);
+    delay(1000);
+}
