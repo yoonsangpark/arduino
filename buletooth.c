6c92e358cab90cb07b89f21339ed962ba97afee3
yoonsangpark <yoonsangpark@gmail.com>
2026-06-13 20:49:10 +0900

    Update buletooth.c

--- buletooth.c
+++ buletooth.c
@@ -5,29 +5,19 @@ SoftwareSerial BT(10, 11); // RX, TX
 void setup()
 {
     Serial.begin(9600);
-    BT.begin(9600);   // HC-06 기본 속도
+    BT.begin(9600);
 
     Serial.println("HC-06 Ready");
 }
 
 void loop()
 {
-    // 스마트폰 -> Arduino
+    // Phone -> Arduino
     if (BT.available())
     {
         char c = BT.read();
 
         Serial.print("RX: ");
         Serial.println(c);
-
-        // 받은 데이터 에코
-        BT.print("Received: ");
-        BT.println(c);
-    }
-
-    // Arduino IDE 시리얼 모니터 -> 스마트폰
-    if (Serial.available())
-    {
-        BT.write(Serial.read());
     }
 }
